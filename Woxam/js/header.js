5adeac5393fc34e4ee2d8c567c21bf5c08178755
Niktia <34923867+NuniQ@users.noreply.github.com>
2019-01-09 00:11:36 +0300

    fix

--- Woxam/js/header.js
+++ Woxam/js/header.js
@@ -3,7 +3,15 @@
 	toggler.addEventListener("click", function () {
 		toggler.classList.toggle("active");
 		$(".header .header__top__navbar").fadeToggle(200);
+		$(".header .header__top .form__search").fadeOut(200);
 	});
+	let togglerSearch = document.querySelector(".header .header__top .func .search");
+	togglerSearch.addEventListener("click", function () {
+		$(".header .header__top .form__search").fadeToggle(200);
+		toggler.classList.remove("active");
+		$(".header .header__top__navbar").fadeOut(200);
+	});
+	
 	$(document).ready(function() {
 		$('.header__slider .slider').slick({
   infinite: true,
