2ac68436b89b1dc25badb6189639ea8c37d0b9f7
Niktia <34923867+NuniQ@users.noreply.github.com>
2019-01-08 22:30:11 +0300

    Header update!
    -search is not working
    -cart is not working

--- Woxam/js/header.js
+++ Woxam/js/header.js
@@ -4,4 +4,24 @@
 		toggler.classList.toggle("active");
 		$(".header .header__top__navbar").fadeToggle(200);
 	});
+	$(document).ready(function() {
+		$('.header__slider .slider').slick({
+  infinite: true,
+  speed:600,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:'<button type="button" class="header__prev header__arrow"></button>',
+  nextArrow:'<button type="button" class="header__next header__arrow"></button>',
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+      		dots: false,
+			arrows: false
+      }
+    }]
+		});
+	});
 }())
